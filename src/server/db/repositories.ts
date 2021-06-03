bd90ca6d84cd51bab1879052d29113e0793b5485
Nicolas Laitan <nlaitan@atixlabs.com>
2021-06-03 12:58:17 -0300

    fix: remove params from configure

--- src/server/db/repositories.ts
+++ src/server/db/repositories.ts
@@ -1,5 +1,3 @@
-import { Sequelize } from 'sequelize';
-import { Environment } from '../utils/environment-parser';
 import * as MessageRepository from './message-repository';
 
 export interface Repositories {
@@ -10,9 +8,8 @@ export interface Repositories {
  * Configures the repositories with the given DB connection to make them ready
  * to be used
  *
- * @param {*} environment connection to be used to run queries
  * @returns {*} message repository
  */
-export const configure = (environment: Environment): Repositories => ({
-  messageRepository: MessageRepository.configure(environment)
+export const configure = (): Repositories => ({
+  messageRepository: MessageRepository.configure()
 });
