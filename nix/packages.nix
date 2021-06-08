281fe97d0775d9e77e8debef681867f68c2dfd42
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2021-06-09 08:57:02 +1000

    nix: Run yarn with nodejs_12.x and fix tool deps in the shell

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,10 +1,10 @@
 { lib, pkgs }:
 
 let
-  #nodePackages = pkgs.callPackage ./node-packages {};
   packages = self: {
     sources = import ./sources.nix;
     nodejs = pkgs.nodejs-12_x;
+    yarn = pkgs.yarn.override { nodejs = self.nodejs; };
     nix-inclusive = pkgs.callPackage "${self.sources.nix-inclusive}/inclusive.nix" {};
     inherit (self.yarn-static.passthru) offlinecache;
     multisig-coordination-server = self.callPackage ./multisig-coordination-server.nix {};
